3d5f7d48b2e2c00d3d410fa7a9dcf0ddb82a746e
furkan soylek <soyleksoylek@gmail.com>
2023-10-12 18:20:19 +0300

    laptop

--- main.py
+++ main.py
@@ -14,21 +14,23 @@ import requests
 from io import BytesIO
 
 
-token = util.prompt_for_user_token('317h99f45vnqujlkyiarzx3mr', 'user-read-currently-playing', '144bc246b721405a8f72b6f92ccead4d', '799c3ada2dba4a958eb152710108c211', 'http://localhost:8888/callback/')
+token = util.prompt_for_user_token('', 'user-read-currently-playing', '144bc246b721405a8f72b6f92ccead4d', '799c3ada2dba4a958eb152710108c211', 'http://localhost:8888/callback/')
 latest=None
-# token='BQCc8Rrx0NPg8TCHIYDAA0B5zerX-YvtQSjxV_qpR_O3-EImk7RNvEr0ynnupJRMDZSJT4uBkkxwsn6JXkgF5VSYG9Y0Xbbe8WZYjDOLM_ycvzqyD4aB5vb2zWFHfrdmK3e3X-RIYRzWkygw_Lxc30N9U8zJFZJL4iTNvgdpbkLrku9Nreqh4DbK7RybTgEFyYXxwiAJ1AkVK5CHtw1Kgw'
 sp = spotipy.Spotify(auth=token)
 print(token)
 
 def refresh_image(canvas, img, image_path, image_id):
+    global sp,latest
     try:
         minw=min(root.winfo_height(),root.winfo_width())
         if minw<150:
             minw=150
-        # print(current_song['item']['album']['images'][0]['url'])
-        # print(current_song['item']['album']['name'])
-        # print(current_song['item']['album']['artists'][0]['name'])
         current_song = sp.currently_playing()
+        # print(current_song)
+        # print(current_song['item']['album']['images'][0]['url'])     -> album image
+        # print(current_song['item']['album']['name'])      -> album name
+        # print(current_song['item']['album']['artists'][0]['name'])    -> artist name
+        # print(current_song['item']['name'])     -> song name
         response = requests.get(current_song['item']['album']['images'][0]['url'])
         img = ImageTk.PhotoImage(Image.open(BytesIO(response.content)).resize((minw,minw), Image.ANTIALIAS))
         canvas.configure(width=minw,height=minw)
@@ -36,18 +38,19 @@ def refresh_image(canvas, img, image_path, image_id):
         canvas.after(1000, refresh_image, canvas, img, image_path, image_id)
     except spotipy.SpotifyException as e:
         print(e)
-        print('token expired!')
-        token = util.prompt_for_user_token('317h99f45vnqujlkyiarzx3mr', 'user-read-currently-playing', '144bc246b721405a8f72b6f92ccead4d', '799c3ada2dba4a958eb152710108c211', 'http://localhost:8888/callback/')
+        print('token changing!')
+        token = util.prompt_for_user_token('', 'user-read-currently-playing', '144bc246b721405a8f72b6f92ccead4d', '799c3ada2dba4a958eb152710108c211', 'http://localhost:8888/callback/')
+        sp = spotipy.Spotify(auth=token)
         img = ImageTk.PhotoImage(Image.open('150.png'))
         canvas.configure(width=minw,height=minw)
         canvas.itemconfigure(image_id, image=img)
         canvas.after(1000, refresh_image, canvas, img, image_path, image_id)
-    # except Exception as e:
-    #     print(e)
-    #     img = ImageTk.PhotoImage(Image.open('150.png'))
-    #     canvas.configure(width=minw,height=minw)
-    #     canvas.itemconfigure(image_id, image=img)
-    #     canvas.after(1000, refresh_image, canvas, img, image_path, image_id)
+    except Exception as e:
+        print(e)
+        img = ImageTk.PhotoImage(Image.open('150.png'))
+        canvas.configure(width=minw,height=minw)
+        canvas.itemconfigure(image_id, image=img)
+        canvas.after(1000, refresh_image, canvas, img, image_path, image_id)
 
 
 
