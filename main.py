d1c43308111018df8425e004a131929ccb033ba1
furkan soylek <soyleksoylek@gmail.com>
2023-10-17 16:42:40 +0300

    lptp

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 from PIL import Image, ImageTk
 import tkinter as tk
-import os
-import shutil
-import threading
-import time
-import sys
 import spotipy
 import spotipy.util as util
-import spotipy.oauth2 as oauth2
-from urllib.request import urlopen
-import io
 import requests
 from io import BytesIO
 
 
-token = util.prompt_for_user_token('', 'user-read-currently-playing', '144bc246b721405a8f72b6f92ccead4d', '799c3ada2dba4a958eb152710108c211', 'http://localhost:8888/callback/')
+
+Client_id='144bc246b721405a8f72b6f92ccead4d'
+Client_secret='799c3ada2dba4a958eb152710108c211'
+
+
+token = util.prompt_for_user_token('', 'user-read-currently-playing', Client_id, Client_secret, 'http://localhost:8888/callback/')
 latest=None
 sp = spotipy.Spotify(auth=token)
 print(token)
 
-def refresh_image(canvas, img, image_path, image_id):
+def refresh_image(canvas, img, image_path, image_id,text_id):
     global sp,latest
     try:
         minw=min(root.winfo_height(),root.winfo_width())
@@ -32,28 +29,29 @@ def refresh_image(canvas, img, image_path, image_id):
         # print(current_song['item']['album']['artists'][0]['name'])    -> artist name
         # print(current_song['item']['name'])     -> song name
         response = requests.get(current_song['item']['album']['images'][0]['url'])
-        img = ImageTk.PhotoImage(Image.open(BytesIO(response.content)).resize((minw,minw), Image.ANTIALIAS))
+        img = ImageTk.PhotoImage(Image.open(BytesIO(response.content)).resize((minw,minw), Image.Resampling.LANCZOS))
         canvas.configure(width=minw,height=minw)
         canvas.itemconfigure(image_id, image=img)
-        canvas.after(1000, refresh_image, canvas, img, image_path, image_id)
+        canvas.itemconfigure(text_id,text=current_song['item']['name'])
+        canvas.moveto(text_id,minw//2,minw//2)
+        canvas.after(1000, refresh_image, canvas, img, image_path, image_id,text_id)
     except spotipy.SpotifyException as e:
         print(e)
         print('token changing!')
-        token = util.prompt_for_user_token('', 'user-read-currently-playing', '144bc246b721405a8f72b6f92ccead4d', '799c3ada2dba4a958eb152710108c211', 'http://localhost:8888/callback/')
+        token = util.prompt_for_user_token('', 'user-read-currently-playing', Client_id, Client_secret, 'http://localhost:8888/callback/')
         sp = spotipy.Spotify(auth=token)
         img = ImageTk.PhotoImage(Image.open('150.png'))
         canvas.configure(width=minw,height=minw)
         canvas.itemconfigure(image_id, image=img)
-        canvas.after(1000, refresh_image, canvas, img, image_path, image_id)
+        canvas.after(1000, refresh_image, canvas, img, image_path, image_id,text_id)
     except Exception as e:
         print(e)
         img = ImageTk.PhotoImage(Image.open('150.png'))
         canvas.configure(width=minw,height=minw)
         canvas.itemconfigure(image_id, image=img)
-        canvas.after(1000, refresh_image, canvas, img, image_path, image_id)
-
-
-
+        canvas.after(1000, refresh_image, canvas, img, image_path, image_id,text_id)
+    except KeyboardInterrupt:
+        pass
 
 root = tk.Tk()
 root.overrideredirect(True)
@@ -75,9 +73,10 @@ image_path = 'placeholder.png'
 canvas = tk.Canvas(root, bg="blue", bd=0, highlightthickness=0,)
 img = None  # initially only need a canvas image place-holder
 image_id = canvas.create_image(0, 0, image=img,anchor="nw")
+text_id = canvas.create_text(75,75,text='SpotiPanel',anchor='center',fill='cyan')
 canvas.bind("<B1-Motion>",move_app)
 canvas.bind("<Double-Button-1>",resize_app_inc)
 canvas.bind("<Double-Button-3>",resize_app_decr)
 canvas.pack()
-refresh_image(canvas, img, image_path, image_id)
+refresh_image(canvas, img, image_path, image_id,text_id)
 root.mainloop()
